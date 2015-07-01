ed1981b63f277ddcf1a3d5e93ab687f4b1e90d0b
Josh Lee <Lee45276@gmail.com>
2015-07-01 18:01:45 +0800

    Created categories model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701094849) do
+ActiveRecord::Schema.define(version: 20150701095951) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text   "body"
