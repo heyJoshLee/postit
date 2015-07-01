ed78588ab300c2ac33fe5c0711c4fd82a1bb0c34
Josh Lee <Lee45276@gmail.com>
2015-07-01 17:35:48 +0800

    create_users migration; Created Users model; Created first User

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701092232) do
+ActiveRecord::Schema.define(version: 20150701093312) do
 
   create_table "posts", force: true do |t|
     t.string "url"
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
   end
 
 end
