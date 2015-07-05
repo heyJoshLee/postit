112663c51cb0d538c991c05999f57f6170347ac4
Josh Lee <Lee45276@gmail.com>
2015-07-05 20:58:25 +0800

    Linked post_categories join table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
-  
+  has_many :post_categories
+  has_many :posts, through: :post_categories
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :users
   has_many :comments
+  has_many :post_categories 
+  has_many :categories, through: :post_categories
 end
