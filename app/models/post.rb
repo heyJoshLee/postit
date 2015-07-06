fd4d02464688a9f52ac7fbd8b30ab0ff2601b235
Josh Lee <Lee45276@gmail.com>
2015-07-06 08:42:30 +0800

    Changed post belongs_to users to belongs_to :creator, foreign_key: 'user_id', class_name: 'User'

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories 
   has_many :categories, through: :post_categories
