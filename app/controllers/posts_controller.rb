e0741682f382feada5e47155ad9d5ad0d483de73
Josh Lee <Lee45276@gmail.com>
2015-07-06 22:08:15 +0800

    Added partical to display categories on post#index and post#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end 
+
+  def update
+
+  end
 end
