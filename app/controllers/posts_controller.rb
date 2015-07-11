cb397b8998ca61eaf1ea891909e477566d2cc9c3
Josh Lee <Lee45276@gmail.com>
2015-07-11 16:56:23 +0800

    Added form to create and update posts. Added link to create post in header. Added edit link to each post in index view. Adder comments and users to each show view for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,34 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
     
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
 
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end 
 
   def update
+    @post = Post.find(params[:id])
 
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,11 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_categories 
   has_many :categories, through: :post_categories
+
+  
+  validates :title, :url, presence: true
+  validates :title, length: {minimum: 2, maximum: 140}
+  validates :description, length: {maximum: 1000}
+  validates :url, length: {minimum: 2}
+
 end
