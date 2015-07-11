fd684af8fdea05da924d0243fa8bf7a6d18abef0
Josh Lee <Lee45276@gmail.com>
2015-07-11 17:09:00 +0800

    Added before_action to posts#show, edit and update to set @post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update]
+  
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,11 +25,16 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end 
 
   def update
-    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Post updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
 
   end
 
@@ -35,6 +42,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit!
+  end
+
+  def set_post
+      @post = Post.find(params[:id])
   end
 
 
