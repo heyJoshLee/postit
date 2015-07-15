fed57ae48e20e6c019e288ec214ef12fd8d1295c
Josh Lee <Lee45276@gmail.com>
2015-07-15 16:19:37 +0800

    Created sessions_controller, routed /login to sessions#new, created new session view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/login', to: "sessions#new"
+
   resources :posts, except: [:destroy] do
     resources :comments
   end
