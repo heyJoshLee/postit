dda2a010a5b6e1872d0d7fd4b903ebdf048943f4
Josh Lee <Lee45276@gmail.com>
2015-07-29 15:11:10 +0800

    Improved slugs for user, category and post

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
   validates :name, presence: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
 
 
@@ -12,7 +12,32 @@ class Category < ActiveRecord::Base
     
   end
 
-  def generate_slug
-    self.slug = self.name.gsub(" ", "-").downcase
+  
+  def generate_slug!
+    the_slug = to_slug(self.name)
+    post = Category.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Category.find_by slug: the_slug
+      count += 1
+    end    
+    self.slug = the_slug.downcase
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
+
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split("-").slice(0...-1).join("-") + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, on: :create, length: {minimum: 5}
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
 
   def to_param
@@ -17,8 +17,32 @@ class User < ActiveRecord::Base
     
   end
 
-  def generate_slug
-    self.slug = self.username.gsub(" ", "-").downcase
+  
+  def generate_slug!
+    the_slug = to_slug(self.username)
+    post = User.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = User.find_by slug: the_slug
+      count += 1
+    end    
+    self.slug = the_slug.downcase
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split("-").slice(0...-1).join("-") + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
   end
 
 
